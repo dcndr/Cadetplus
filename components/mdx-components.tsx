f6202412e30e0c76b07330574157375b3bfc5edf
Darren Candra <darrencndr@gmail.com>
2024-02-21 19:34:42 +1100

    remove more useless

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import { useMDXComponent } from "next-contentlayer/hooks"
 
 const components = {
   Image,
@@ -7,10 +6,4 @@ const components = {
 
 interface MdxProps {
   code: string
-}
-
-export function Mdx({ code }: MdxProps) {
-  const Component = useMDXComponent(code)
-
-  return <Component components={components} />
 }
