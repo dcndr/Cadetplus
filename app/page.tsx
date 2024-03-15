f1c93eb458ed3b51212f2b3687a5d56653a5f199
Darren Candra <darrencndr@gmail.com>
2024-03-16 00:39:28 +1100

    update spacings

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,13 @@ export default function Home() {
             <span className="text-lg">Generate perfect, lightning-fast leave forms, every time.</span>
           </div>
         </a>
-        <span className="pt-8">Cadetplus is designed for however is not affiliated or endorsed by BHHSACU.</span>
-        <span className="opacity-50">All forms are public and freely available at <a href="https://bhhsacu.org">bhhsacu.org</a></span>
-        <span className="opacity-50">Any problems lmk <a href="mailto:cadetplus@candra.dev">cadetplus@candra.dev</a></span>
+        <div className="flex flex-col pt-8 gap-0.5">
+          <span>Cadetplus is designed for however is not affiliated or endorsed by BHHSACU.</span>
+          <div className="flex flex-col opacity-50">
+            <span>All forms are public and freely available at <a href="https://bhhsacu.org">bhhsacu.org</a></span>
+            <span className="-translate-y-1">Any problems lmk <a href="mailto:cadetplus@candra.dev">cadetplus@candra.dev</a></span>
+          </div>
+        </div>
       </div>
     </div>
   )
