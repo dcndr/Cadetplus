e0c33031928bf13a17e06b2e671c2437b3dd837f
Darren Candra <darrencndr@gmail.com>
2024-03-16 00:26:08 +1100

    remove paddings

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
           </div>
         </a>
         <span className="pt-8">Cadetplus is designed for however is not affiliated or endorsed by BHHSACU.</span>
-        <span className="opacity-50 pt-1.5">All forms are public and freely available at <a href="https://bhhsacu.org">bhhsacu.org</a></span>
-        <span className="opacity-50 pt-1.5">Any problems lmk <a href="mailto:cadetplus@candra.dev">cadetplus@candra.dev</a></span>
+        <span className="opacity-50">All forms are public and freely available at <a href="https://bhhsacu.org">bhhsacu.org</a></span>
+        <span className="opacity-50">Any problems lmk <a href="mailto:cadetplus@candra.dev">cadetplus@candra.dev</a></span>
       </div>
     </div>
   )
